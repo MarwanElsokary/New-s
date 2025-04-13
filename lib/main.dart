7c2ab6afe45ba817a7c7441447a352297e380abe
ahmed <atef87133@gmail.com>
2025-04-13 04:12:25 +0200

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("kosmk")));
+    return MaterialApp();
   }
 }
